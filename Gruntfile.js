bd29a056fdb75028bcd63a756fe0d5bc7d93170d
Voles <niels.dequeker@gmail.com>
2014-02-07 23:10:05 +0100

    Setup Karma configuration for tests + added initial test

--- Gruntfile.js
+++ Gruntfile.js
@@ -111,12 +111,27 @@ module.exports = function (grunt) {
             }
         },
 
+        // karma
+        karma: {
+            options: {
+                configFile: 'karma.conf.js',
+                autoWatch: true
+            },
+
+            single: {
+                singleRun: true
+            },
+
+            continuous: {
+                singleRun: false
+            }
+        }
     });
 
     // default
     grunt.registerTask('default', []);
-
-    grunt.registerTask('build', ['jshint:source', 'clean:build', 'concat:build', 'uglify:build']);
-
+    grunt.registerTask('build', ['jshint:source', 'karma:single', 'clean:build', 'concat:build', 'uglify:build']);
     grunt.registerTask('webserver', ['build', 'open', 'connect:demo', 'watch']);
+    grunt.registerTask('test', ['karma:single']);
+    grunt.registerTask('test:continuous', ['karma:continuous']);
 };
