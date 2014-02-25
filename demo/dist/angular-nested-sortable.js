aeed495db5837ea0358806aedc1713cd04c9f49c
Voles <niels.dequeker@gmail.com>
2014-02-25 10:53:11 +0100

    New build

--- demo/dist/angular-nested-sortable.js
+++ demo/dist/angular-nested-sortable.js
@@ -270,7 +270,7 @@
   angular.module('ui.nestedSortable')
 
     .directive('uiNestedSortable', [ 'nestedSortableConfig', '$window',
-      function(nestedSortableConfig, $window) {
+      function(nestedSortableConfig) {
         return {
           require: ['ngModel', '?^uiNestedSortableItem'],
           restrict: 'A',
@@ -319,6 +319,10 @@
             };
 
             callbacks.itemMoved = function(sourceScope, sourceItem, sourceIndex, destScope, destIndex) {
+
+            };
+
+            callbacks.itemClicked = function(sourceItem) {
 
             };
 
@@ -385,6 +389,9 @@
                 return;
               }
 
+              var sourceItem = angular.element(e.target).scope().itemData();
+              scope.callbacks.itemClicked(sourceItem);
+
               var target = angular.element(e.target);
               var nodrag = function (targetElm) {
                 return (typeof targetElm.attr('nodrag')) != "undefined"
@@ -396,7 +403,7 @@
                 }
                 target = target.parent();
               }
-                  
+
               var moveObj = e;
               if (hasTouch) {
                 if (e.targetTouches !== undefined) {
@@ -502,7 +509,7 @@
                 });
 
                 $helper.positionMoved(e, pos, firstMoving);
-                
+
                 if (firstMoving) {
                   firstMoving = false;
                   return;
@@ -574,7 +581,7 @@
                 if (targetItem) {
                   targetItemData = targetItem.itemData();
                 }
-                  
+
                 currentAccept = targetItem.accept(scope);
 
                 // move vertical
@@ -642,8 +649,7 @@
 
                   if (sameParent) {
                     scope.callbacks.orderChanged(scope.sortableElement.scope(), source, sourceIndex, destIndex);
-                  }
-                  else {
+                  } else {
                     scope.callbacks.itemRemoved(scope.sortableElement.scope(), source, sourceIndex);
                     targetScope.callbacks.itemAdded(targetScope, source, destIndex);
                     scope.callbacks.itemMoved(scope.sortableElement.scope(), source, sourceIndex, targetScope, destIndex);
