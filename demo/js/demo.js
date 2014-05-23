5675c868ee79b7a05f05b3c8f8d60c70515fd741
Jim Liu <junminliu@gmail.com>
2014-05-23 23:09:47 +0800

    roll back dem.'s

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -3,13 +3,30 @@
 
   angular.module('demoApp', ['ui.tree'])
   .controller('MainCtrl', function($scope) {
-    $scope.list =  [{
+    $scope.list = [{
       "id": 1,
       "title": "1. dragon-breath",
       "items": []
     }, {
       "id": 2,
       "title": "2. moiré-vision",
+      "items": [{
+        "id": 21,
+        "title": "2.1. tofu-animation",
+        "items": [{
+          "id": 211,
+          "title": "2.1.1. spooky-giraffe",
+          "items": []
+        }, {
+          "id": 212,
+          "title": "2.1.2. bubble-burst",
+          "items": []
+        }],
+      }, {
+        "id": 22,
+        "title": "2.2. barehand-atomsplitting",
+        "items": []
+      }],
     }, {
       "id": 3,
       "title": "3. unicorn-zapper",
