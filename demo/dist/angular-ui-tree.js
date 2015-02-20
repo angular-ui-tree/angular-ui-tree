74aae11a95976abc6036e3b520218cba85f51da3
SergeyTokarev <seregat1984@gmail.com>
2015-02-20 18:33:03 +0200

    #372 Expand subtree on drag over after 500ms.

--- demo/dist/angular-ui-tree.js
+++ demo/dist/angular-ui-tree.js
@@ -966,6 +966,11 @@
                 return;
               }
 
+              if (dragInfo && dragInfo.$standingTimeout) {
+                $timeout.cancel(dragInfo.$standingTimeout);
+                dragInfo.$standingTimeout = null;
+              }
+
               var eventObj = $uiTreeHelper.eventObj(e);
               var prev, leftElmPos, topElmPos;
 
@@ -1108,6 +1113,13 @@
                     treeScope = null;
                   }
 
+                  // Init timeout to expand node
+                  if (targetNode.$type == 'uiTreeNode') {
+                    dragInfo.$standingTimeout = $timeout(function () {
+                      targetNode.expand();
+                    }, 500);
+                  }
+
                   if (isEmpty) { // it's an empty tree
                     treeScope = targetNode;
                     if (targetNode.$nodesScope.accept(scope, 0)) {
@@ -1168,7 +1180,13 @@
                   scope.$callbacks.dragStop(dragInfo.eventArgs(elements, pos));
                 });
                 scope.$$apply = false;
+
+                if (dragInfo && dragInfo.$standingTimeout) {
+                  $timeout.cancel(dragInfo.$standingTimeout);
+                }
+
                 dragInfo = null;
+
 
               }
 
