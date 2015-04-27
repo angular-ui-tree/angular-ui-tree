ffbda69d5802bdeadfbda2302f44ef50087ae34a
Niels Dequeker <niels.dequeker@gmail.com>
2015-04-27 20:43:03 +0200

    New build

--- dist/angular-ui-tree.js
+++ dist/angular-ui-tree.js
@@ -358,128 +358,128 @@
   'use strict';
 
   angular.module('ui.tree')
-    .directive('uiTree', [ 'treeConfig', '$window',
-      function(treeConfig, $window) {
-        return {
-          restrict: 'A',
-          scope: true,
-          controller: 'TreeController',
-          link: function(scope, element, attrs) {
-            var callbacks = {
-              accept: null,
-              beforeDrag: null
-            };
+  .directive('uiTree', [ 'treeConfig', '$window',
+    function(treeConfig, $window) {
+      return {
+        restrict: 'A',
+        scope: true,
+        controller: 'TreeController',
+        link: function(scope, element, attrs) {
+          var callbacks = {
+            accept: null,
+            beforeDrag: null
+          };
 
-            var config = {};
-            angular.extend(config, treeConfig);
-            if (config.treeClass) {
-              element.addClass(config.treeClass);
-            }
-
-            scope.$emptyElm = angular.element($window.document.createElement('div'));
-            if (config.emptyTreeClass) {
-              scope.$emptyElm.addClass(config.emptyTreeClass);
-            }
-
-            scope.$watch('$nodesScope.$modelValue.length', function() {
-              if (scope.$nodesScope.$modelValue) {
-                scope.resetEmptyElement();
-              }
-            }, true);
-
-            scope.$watch(attrs.dragEnabled, function(val) {
-              if((typeof val) == "boolean") {
-                scope.dragEnabled = val;
-              }
-            });
-
-            scope.$watch(attrs.emptyPlaceHolderEnabled, function(val) {
-              if((typeof val) == "boolean") {
-                scope.emptyPlaceHolderEnabled = val;
-              }
-            });
-
-            scope.$watch(attrs.nodropEnabled, function(val) {
-              if((typeof val) == "boolean") {
-                scope.nodropEnabled = val;
-              }
-            });
-
-            scope.$watch(attrs.cloneEnabled, function(val) {
-              if((typeof val) == "boolean") {
-                scope.cloneEnabled = val;
-              }
-            });
-
-            scope.$watch(attrs.maxDepth, function(val) {
-              if((typeof val) == "number") {
-                scope.maxDepth = val;
-              }
-            });
-
-            scope.$watch(attrs.dragDelay, function(val) {
-              if((typeof val) == "number") {
-                scope.dragDelay = val;
-              }
-            });
-
-            // check if the dest node can accept the dragging node
-            // by default, we check the 'data-nodrop-enabled' attribute in `ui-tree-nodes`
-            // and the 'max-depth' attribute in `ui-tree` or `ui-tree-nodes`.
-            // the method can be overrided
-            callbacks.accept = function(sourceNodeScope, destNodesScope, destIndex) {
-              if (destNodesScope.nodropEnabled || destNodesScope.outOfDepth(sourceNodeScope)) {
-                return false;
-              }
-              return true;
-            };
-
-            callbacks.beforeDrag = function(sourceNodeScope) {
-              return true;
-            };
-
-            callbacks.removed = function(node){
-
-            };
-
-            callbacks.dropped = function(event) {
-
-            };
-
-            //
-            callbacks.dragStart = function(event) {
-
-            };
-
-            callbacks.dragMove = function(event) {
-
-            };
-
-            callbacks.dragStop = function(event) {
-
-            };
-
-            callbacks.beforeDrop = function(event) {
-
-            };
-
-            scope.$watch(attrs.uiTree, function(newVal, oldVal){
-              angular.forEach(newVal, function(value, key){
-                if (callbacks[key]) {
-                  if (typeof value === "function") {
-                    callbacks[key] = value;
-                  }
-                }
-              });
-
-              scope.$callbacks = callbacks;
-            }, true);
-
-
+          var config = {};
+          angular.extend(config, treeConfig);
+          if (config.treeClass) {
+            element.addClass(config.treeClass);
           }
-        };
-      }
-    ]);
+
+          scope.$emptyElm = angular.element($window.document.createElement('div'));
+          if (config.emptyTreeClass) {
+            scope.$emptyElm.addClass(config.emptyTreeClass);
+          }
+
+          scope.$watch('$nodesScope.$modelValue.length', function() {
+            if (scope.$nodesScope.$modelValue) {
+              scope.resetEmptyElement();
+            }
+          }, true);
+
+          scope.$watch(attrs.dragEnabled, function(val) {
+            if((typeof val) == "boolean") {
+              scope.dragEnabled = val;
+            }
+          });
+
+          scope.$watch(attrs.emptyPlaceHolderEnabled, function(val) {
+            if((typeof val) == "boolean") {
+              scope.emptyPlaceHolderEnabled = val;
+            }
+          });
+
+          scope.$watch(attrs.nodropEnabled, function(val) {
+            if((typeof val) == "boolean") {
+              scope.nodropEnabled = val;
+            }
+          });
+
+          scope.$watch(attrs.cloneEnabled, function(val) {
+            if((typeof val) == "boolean") {
+              scope.cloneEnabled = val;
+            }
+          });
+
+          scope.$watch(attrs.maxDepth, function(val) {
+            if((typeof val) == "number") {
+              scope.maxDepth = val;
+            }
+          });
+
+          scope.$watch(attrs.dragDelay, function(val) {
+            if((typeof val) == "number") {
+              scope.dragDelay = val;
+            }
+          });
+
+          // check if the dest node can accept the dragging node
+          // by default, we check the 'data-nodrop-enabled' attribute in `ui-tree-nodes`
+          // and the 'max-depth' attribute in `ui-tree` or `ui-tree-nodes`.
+          // the method can be overrided
+          callbacks.accept = function(sourceNodeScope, destNodesScope, destIndex) {
+            if (destNodesScope.nodropEnabled || destNodesScope.outOfDepth(sourceNodeScope)) {
+              return false;
+            }
+            return true;
+          };
+
+          callbacks.beforeDrag = function(sourceNodeScope) {
+            return true;
+          };
+
+          callbacks.removed = function(node){
+          
+          };
+
+          callbacks.dropped = function(event) {
+
+          };
+
+          //
+          callbacks.dragStart = function(event) {
+
+          };
+
+          callbacks.dragMove = function(event) {
+
+          };
+
+          callbacks.dragStop = function(event) {
+
+          };
+
+          callbacks.beforeDrop = function(event) {
+
+          };
+
+          scope.$watch(attrs.uiTree, function(newVal, oldVal){
+            angular.forEach(newVal, function(value, key){
+              if (callbacks[key]) {
+                if (typeof value === "function") {
+                  callbacks[key] = value;
+                }
+              }
+            });
+
+            scope.$callbacks = callbacks;
+          }, true);
+
+
+        }
+      };
+    }
+  ]);
 })();
 
 (function () {
@@ -941,64 +941,64 @@
   'use strict';
 
   angular.module('ui.tree')
-    .directive('uiTreeNodes', [ 'treeConfig', '$window',
-      function(treeConfig) {
-        return {
-          require: ['ngModel', '?^uiTreeNode', '^uiTree'],
-          restrict: 'A',
-          scope: true,
-          controller: 'TreeNodesController',
-          link: function(scope, element, attrs, controllersArr) {
+  .directive('uiTreeNodes', [ 'treeConfig', '$window',
+    function(treeConfig) {
+      return {
+        require: ['ngModel', '?^uiTreeNode', '^uiTree'],
+        restrict: 'A',
+        scope: true,
+        controller: 'TreeNodesController',
+        link: function(scope, element, attrs, controllersArr) {
 
-            var config = {};
-            angular.extend(config, treeConfig);
-            if (config.nodesClass) {
-              element.addClass(config.nodesClass);
-            }
-
-            var ngModel = controllersArr[0];
-            var treeNodeCtrl = controllersArr[1];
-            var treeCtrl = controllersArr[2];
-            if (treeNodeCtrl) {
-              treeNodeCtrl.scope.$childNodesScope = scope;
-              scope.$nodeScope = treeNodeCtrl.scope;
-            }
-            else { // find the root nodes if there is no parent node and have a parent ui-tree
-              treeCtrl.scope.$nodesScope = scope;
-            }
-            scope.$treeScope = treeCtrl.scope;
-
-            if (ngModel) {
-              ngModel.$render = function() {
-                if (!ngModel.$modelValue || !angular.isArray(ngModel.$modelValue)) {
-                  scope.$modelValue = [];
-                }
-                scope.$modelValue = ngModel.$modelValue;
-              };
-            }
-
-            scope.$watch(attrs.maxDepth, function(val) {
-              if((typeof val) == "number") {
-                scope.maxDepth = val;
-              }
-            });
-
-            scope.$watch(function () {
-              return attrs.nodropEnabled;
-            }, function (newVal) {
-              if((typeof newVal) != "undefined") {
-                scope.nodropEnabled = true;
-              }
-            }, true);
-
-            attrs.$observe('horizontal', function(val) {
-              scope.horizontal = ((typeof val) != "undefined");
-            });
-
+          var config = {};
+          angular.extend(config, treeConfig);
+          if (config.nodesClass) {
+            element.addClass(config.nodesClass);
           }
-        };
-      }
-    ]);
+
+          var ngModel = controllersArr[0];
+          var treeNodeCtrl = controllersArr[1];
+          var treeCtrl = controllersArr[2];
+          if (treeNodeCtrl) {
+            treeNodeCtrl.scope.$childNodesScope = scope;
+            scope.$nodeScope = treeNodeCtrl.scope;
+          }
+          else { // find the root nodes if there is no parent node and have a parent ui-tree
+            treeCtrl.scope.$nodesScope = scope;
+          }
+          scope.$treeScope = treeCtrl.scope;
+
+          if (ngModel) {
+            ngModel.$render = function() {
+              if (!ngModel.$modelValue || !angular.isArray(ngModel.$modelValue)) {
+                scope.$modelValue = [];
+              }
+              scope.$modelValue = ngModel.$modelValue;
+            };
+          }
+
+          scope.$watch(attrs.maxDepth, function(val) {
+            if((typeof val) == "number") {
+              scope.maxDepth = val;
+            }
+          });
+
+          scope.$watch(function () {
+            return attrs.nodropEnabled;
+          }, function (newVal) {
+            if((typeof newVal) != "undefined") {
+              scope.nodropEnabled = true;
+            }
+          }, true);
+
+          attrs.$observe('horizontal', function(val) {
+            scope.horizontal = ((typeof val) != "undefined");
+          });
+
+        }
+      };
+    }
+  ]);
 })();
 
 (function () {
