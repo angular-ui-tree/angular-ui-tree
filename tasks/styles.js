15778880b79449c023996842c001af7d9b004fc0
Niels Dequeker <niels.dequeker@gmail.com>
2015-04-27 20:24:36 +0200

    Remove Gulp Sass dependency

--- tasks/styles.js
+++ tasks/styles.js
@@ -3,11 +3,8 @@
 module.exports = function (gulp, $) {
 
   gulp.task('styles', function () {
-    return gulp.src('source/angular-ui-tree.scss')
-      .pipe($.sass({
-        errLogToConsole: true,
-        outputStyle: 'compressed'
-      }))
+    return gulp.src('source/*.css')
+      .pipe($.minifyCss({ compatibility: 'ie8' }))
       .pipe($.rename('angular-ui-tree.min.css'))
       .pipe(gulp.dest('dist'));
   });
