0f49f2768aa2eb84f365e0cdd84f32049cb82750
Vitaliy Zurian <thecatontheflat@users.noreply.github.com>
2015-11-15 20:57:13 +0100

    Cannot read property 'childNodes' of undefined
    Extra check for recursive call of the countSubTreeDepth

--- dist/angular-ui-tree.js
+++ dist/angular-ui-tree.js
@@ -160,6 +160,7 @@
         *   result is 0 (it has no subtree).
         */
         function countSubTreeDepth(scope) {
+          if (!scope) return 0;
           var thisLevelDepth = 0,
               childNodes = scope.childNodes(),
               childNode,
