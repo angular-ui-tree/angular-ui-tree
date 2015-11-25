7b28aaa8563d286ab40f9b1b8cc4d033099c19cb
zach lysobey <zach@lysobey.com>
2015-11-25 18:24:54 -0500

    clean up page-object and usage in tests

--- e2e/basic-example/page.js
+++ e2e/basic-example/page.js
@@ -1,45 +1,39 @@
 var BASIC_EXAMPLE_URL = 'http://localhost:9000/#/basic-example';
 
-var LOCATORS = {
-  firstSubNodes: by.css([
-          '#tree-root',
-          '.angular-ui-tree-nodes',
-          '.angular-ui-tree-node:first-child',
-          '.angular-ui-tree-nodes',
-          '.angular-ui-tree-node'
-      ].join(' > ')
-    ),
-  topLevelNodeRepeaters: by.repeater('node in data'),
-  childNodeRepeaters: by.repeater('node in node.nodes'),
-  nodeHandles: by.css('[ui-tree-handle]')
-}
-
 var BasicExamplePageNode = function (nodeLocation) {
-  this._node = element(by.xpath(xpathStringForNodeAtPosition(nodeLocation)));
-  this._handles = this._node.all(LOCATORS.nodeHandles);
-  this.getHandle = function() { return this._handles.first(); };
-  this.getText = this._handles.first().getText;
+  var subnodesXpath = 'ol/li[contains(@class,"angular-ui-tree-node")]';
+  var nodeHandlesLocator = by.css('[ui-tree-handle]');
+
+  var nodeElement = element(by.xpath(xpathStringForNodeAtPosition(nodeLocation)));
+  var handle = nodeElement.all(nodeHandlesLocator).first();
+
+  this.getElement = function() { return nodeElement; }
+  this.getHandle = function() { return handle; };
+  this.getText = handle.getText;
+  this.getSubnodes = function() {
+    return nodeElement.all(by.xpath(subnodesXpath));
+  }
 
   function xpathStringForNodeAtPosition(nodeLocation) {
     var xpathChunks = ['//*[@id="tree-root"]'];
     nodeLocation.forEach(function(index) {
-      xpathChunks.push('ol/li[contains(@class,"angular-ui-tree-node")][' + index + ']')
+      xpathChunks.push(subnodesXpath + '[' + index + ']')
     });
     return xpathChunks.join('/');
   }
 }
 
 var BasicExamplePage = function () {
-  this.tree = element(by.css('#tree-root'));
-  this.firstSubNodes = element.all(LOCATORS.firstSubNodes);
-  this.rootNodes = element.all(LOCATORS.topLevelNodeRepeaters);
+  this.get = function () {
+    browser.get(BASIC_EXAMPLE_URL);
+  };
+
+  this.getRootNodes = function() {
+    return element.all(by.repeater('node in data'));
+  };
 
   this.getNodeAtPosition = function () {
     return new BasicExamplePageNode([].slice.call(arguments));
-  }
-
-  this.get = function () {
-    browser.get(BASIC_EXAMPLE_URL);
   };
 };
 
