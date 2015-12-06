a52e95010d041b0390338f3e83a3c07a563df584
Daniel Tony <dani3l@gmail.com>
2015-12-06 14:54:40 +0200

    Add support for right-to-left
    Add support for RTL trees and determine level direction

--- source/directives/uiTreeNode.js
+++ source/directives/uiTreeNode.js
@@ -312,9 +312,15 @@
                 // move horizontal
                 if (pos.dirAx && pos.distAxX >= config.levelThreshold) {
                   pos.distAxX = 0;
+                  
+                  // check if document is rtl or ltr and determine level direction
+                  var isRTL = angular.element('html[dir="rtl"]').length > 0;
+
+                  var increaseLevel = (isRTL) ? pos.distX < 0 : pos.distX > 0;
+                  var decreaseLevel = (isRTL) ? pos.distX > 0 : pos.distX < 0;
 
                   // increase horizontal level if previous sibling exists and is not collapsed
-                  if (pos.distX > 0) {
+                  if (increaseLevel) {
                     prev = dragInfo.prev();
                     if (prev && !prev.collapsed
                       && prev.accept(scope, prev.childNodesCount())) {
@@ -324,7 +330,7 @@
                   }
 
                   // decrease horizontal level
-                  if (pos.distX < 0) {
+                  if (decreaseLevel) {
                     // we can't decrease a level if an item preceeds the current one
                     next = dragInfo.next();
                     if (!next) {
