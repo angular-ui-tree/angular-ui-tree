0a05b73f478c142674409ca5fab5c64464d22178
zach lysobey <zach@lysobey.com>
2015-12-21 20:30:22 -0500

    create e2e tests for table-example

---
+++ e2e/table-example/page.js
@@ -0,0 +1,29 @@
+var TABLE_EXAMPLE_URL = 'http://localhost:9000/#/table-example';
+
+var TableExamplePageNode = function (position) {
+  var nodesXpath = '//*[@id="tree-root"]/tbody/tr[contains(@class,"angular-ui-tree-node")]';
+  var thisNodeXpath = nodesXpath + '[' + position + ']';
+  var nodeHandlesLocator = by.css('[ui-tree-handle]');
+  var nodeElement = element(by.xpath(thisNodeXpath));
+  var handle = nodeElement.all(nodeHandlesLocator).first();
+
+  this.getElement = function () { return nodeElement; }
+  this.getHandle = function () { return handle; };
+  this.getText = nodeElement.getText;
+}
+
+var TableExamplePage = function () {
+  this.get = function () {
+    browser.get(TABLE_EXAMPLE_URL);
+  };
+
+  this.getRootNodes = function () {
+    return element.all(by.repeater('node in data'));
+  };
+
+  this.getNodeAtPosition = function (position) {
+    return new TableExamplePageNode(position);
+  };
+};
+
+module.exports = new TableExamplePage();
