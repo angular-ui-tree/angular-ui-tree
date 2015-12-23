6157e1ae641c236253189d3667732254ebeccb4e
Niels Dequeker <niels.dequeker@gmail.com>
2015-12-23 11:12:19 +0100

    #684 Include the CSS source files in the build

--- tasks/styles.js
+++ tasks/styles.js
@@ -2,6 +2,11 @@
 
 module.exports = function (gulp, $) {
 
+  gulp.task('styles:copy-source', function () {
+    return gulp.src('source/*.css')
+      .pipe(gulp.dest('dist'));
+  });
+
   gulp.task('styles', function () {
     return gulp.src('source/*.css')
       .pipe($.minifyCss({ compatibility: 'ie8' }))
