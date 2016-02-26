ccc26d3485c08c2e5c6a1dbf8fc6670a6ab78d7a
Karol Lewandowski <karol86@gmail.com>
2016-02-26 09:19:08 +0100

    #695: minor code formatting fix

--- e2e/matchers/customMatchers.js
+++ e2e/matchers/customMatchers.js
@@ -2,33 +2,33 @@ var customMatchers = {
 
   toHaveLogErrors: function (util, customEqualityTesters) {
 
-      function compare(actualLogs, isNegativeComparison) {
-        var i, logEntry,
-            result = {},
-            errorsString = '';
+    function compare(actualLogs, isNegativeComparison) {
+      var i, logEntry,
+          result = {},
+          errorsString = '';
 
-        for (i = 0; i < actualLogs.length; i++) {
-          logEntry = actualLogs[i];
-          if (logEntry.level.value > 900) { // warn or higher level
-            errorsString += ('[' + logEntry.level.name + '] ' + logEntry.message + '\n');
-          }
+      for (i = 0; i < actualLogs.length; i++) {
+        logEntry = actualLogs[i];
+        if (logEntry.level.value > 900) { // warn or higher level
+          errorsString += ('[' + logEntry.level.name + '] ' + logEntry.message + '\n');
         }
-
-        var errorsFound = errorsString.length > 0;
-        result.pass = isNegativeComparison ? !errorsFound : errorsFound;
-
-        var message = 'Expected browser logs ' + (isNegativeComparison ? 'NOT ': '') + 'to have errors';
-        if (!isNegativeComparison) {
-          result.message = message;
-        } else {
-          if (errorsFound) {
-            result.message = message + ' but found:\n' + errorsString;
-          } else {
-            result.message = message;
-          }
-        }
-        return result;
       }
+
+      var errorsFound = errorsString.length > 0;
+      result.pass = isNegativeComparison ? !errorsFound : errorsFound;
+
+      var message = 'Expected browser logs ' + (isNegativeComparison ? 'NOT ': '') + 'to have errors';
+      if (!isNegativeComparison) {
+        result.message = message;
+      } else {
+        if (errorsFound) {
+          result.message = message + ' but found:\n' + errorsString;
+        } else {
+          result.message = message;
+        }
+      }
+      return result;
+    }
 
     return {
       compare: function (actualLogs) {
