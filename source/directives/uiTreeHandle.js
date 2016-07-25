abe9b803bc77244e7870557bd89e1de60863e029
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2016-07-25 15:27:30 +0100

    Allow passing of object to uiTreeHandle
    This object is stored within the uiTreeHandle scope and can be used for
    identifying that handle uniquely (for example, when assigning an action
    to that handle)

--- source/directives/uiTreeHandle.js
+++ source/directives/uiTreeHandle.js
@@ -15,6 +15,11 @@
             if (config.handleClass) {
               element.addClass(config.handleClass);
             }
+            // Store any passed properties in the parent node
+            // object
+            if (attrs.uiTreeHandle.length > 0) {
+              scope.passedObj = scope.$eval(attrs.uiTreeHandle);
+            }
             // connect with the tree node.
             if (scope != treeNodeCtrl.scope) {
               scope.$nodeScope = treeNodeCtrl.scope;
